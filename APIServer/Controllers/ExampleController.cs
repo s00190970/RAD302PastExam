8e4d1367f6543b6c33838fbc02922cce332eac94
Cristina Mihaila <S00184388@mail.itsligo.ie>
2019-03-29 17:00:23 +0000

    S00184388 Part 1b

---
+++ APIServer/Controllers/ExampleController.cs
@@ -0,0 +1,47 @@
+﻿using APIServer.DAL;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APIServer.Controllers
+{
+    public class ExampleController : ApiController
+    {
+        private Example repository;
+
+        public ExampleController()
+        {
+            repository = new Example(new ModelContext ());
+        }
+        // GET: api/Example
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Example/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Example
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Example/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Example/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ APIServer/DAL/Example.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Models;
+
+namespace APIServer.DAL
+{
+    public class Example : IStudentRepository
+    {
+        private ModelContext context;
+        public Example(ModelContext cont)
+        {
+            context = cont;
+        }
+        public Student Add(Student item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Edit(Student item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Student> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student GetById(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(string Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
